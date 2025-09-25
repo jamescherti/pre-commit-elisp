34c2f1e7d1ae3e99c268ca6137f6b7f3ee029d30
James Cherti <60946298+jamescherti@users.noreply.github.com>
2025-09-25 13:30:28 -0400

    Rename test functions and variables

--- tests/test_pre_commit_elisp.py
+++ tests/test_pre_commit_elisp.py
@@ -7,14 +7,14 @@ from pathlib import Path
 
 import pytest
 
-TEST_FILES = Path("./tests/files")
-CHECK_PARENS_ERROR = TEST_FILES / "check_parens_error.el"
-GOOD = TEST_FILES / "good.el"
+TEST_FILES_DIRECTORY = Path("./tests/files")
+FAILURE_FILE = TEST_FILES_DIRECTORY / "check_parens_error.el"
+SUCCESS_FILE = TEST_FILES_DIRECTORY / "good.el"
 
-ELISP_BYTE_COMPILE = Path("hooks/elisp-byte-compile.py")
-ELISP_CHECK_BYTE_COMPILE = Path("hooks/elisp-check-byte-compile.py")
-ELISP_CHECK_PARENS = Path("hooks/elisp-check-parens.py")
-ELISP_INDENT = Path("hooks/elisp-indent.py")
+SCRIPT_BYTE_COMPILE = Path("hooks/elisp-byte-compile.py")
+SCRIPT_CHECK_BYTE_COMPILE = Path("hooks/elisp-check-byte-compile.py")
+SCRIPT_CHECK_PARENS = Path("hooks/elisp-check-parens.py")
+SCRIPT_INDENT = Path("hooks/elisp-indent.py")
 
 
 def run_hook(script: Path, *files: Path):
@@ -25,50 +25,67 @@ def run_hook(script: Path, *files: Path):
     print(result.stderr)
     return result
 
+# -----------------------------------------------------------------------------
+# CHECK BYTE COMPILE
+# -----------------------------------------------------------------------------
 
-def test_check_byte_compile_good():
+
+def test_check_byte_compile_success():
     """Byte-compile a good Elisp file."""
-    result = run_hook(ELISP_CHECK_BYTE_COMPILE, GOOD)
+    result = run_hook(SCRIPT_CHECK_BYTE_COMPILE, SUCCESS_FILE)
     assert result.returncode == 0
 
 
-def test_check_byte_compile_error():
+def test_check_byte_compile_failure():
     """Byte-compile a file that should fail (if any)."""
-    result = run_hook(ELISP_CHECK_BYTE_COMPILE, CHECK_PARENS_ERROR)
+    result = run_hook(SCRIPT_CHECK_BYTE_COMPILE, FAILURE_FILE)
     assert result.returncode != 0
 
+# -----------------------------------------------------------------------------
+# BYTE COMPILE
+# -----------------------------------------------------------------------------
 
-def test_byte_compile_good():
+
+def test_byte_compile_success():
     """Byte-compile a good Elisp file."""
-    elc_file = Path(str(GOOD) + "c")
+    elc_file = Path(str(SUCCESS_FILE) + "c")
     if elc_file.exists():
         os.unlink(elc_file)
 
-    result = run_hook(ELISP_BYTE_COMPILE, GOOD)
+    result = run_hook(SCRIPT_BYTE_COMPILE, SUCCESS_FILE)
     assert result.returncode == 0
-    assert elc_file.exists()  # elc file
+    assert elc_file.exists()
 
 
-def test_byte_compile_error():
+def test_byte_compile_failure():
     """Byte-compile a file that should fail (if any)."""
-    result = run_hook(ELISP_BYTE_COMPILE, CHECK_PARENS_ERROR)
+    result = run_hook(SCRIPT_BYTE_COMPILE, FAILURE_FILE)
     assert result.returncode != 0
 
 
-def test_check_parens_good():
+# -----------------------------------------------------------------------------
+# BYTE COMPILE
+# -----------------------------------------------------------------------------
+
+def test_check_parens_success():
     """Check parentheses on a correct file."""
-    result = run_hook(ELISP_CHECK_PARENS, GOOD)
+    result = run_hook(SCRIPT_CHECK_PARENS, SUCCESS_FILE)
     assert result.returncode == 0
 
 
-def test_check_parens_check_parens_error():
+def test_check_parens_failure():
     """Check parentheses on a correct file."""
-    result = run_hook(ELISP_CHECK_PARENS, CHECK_PARENS_ERROR)
+    result = run_hook(SCRIPT_CHECK_PARENS, FAILURE_FILE)
     assert result.returncode != 0
 
 
-def test_indent_good():
+# -----------------------------------------------------------------------------
+# INDENT
+# -----------------------------------------------------------------------------
+
+
+def test_indent():
     """Run indent hook on a good file."""
     # TODO: Check the content of the file after indenting it
-    result = run_hook(ELISP_INDENT, GOOD)
+    result = run_hook(SCRIPT_INDENT, SUCCESS_FILE)
     assert result.returncode == 0
